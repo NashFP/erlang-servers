31ff54d3261e3864554c1382dbde8f6f31298052
Bryan Hunter <bryan.hunter@fireflylogic.com>
2012-10-30 16:15:50 -0500

    Added a raw solution to simple-atm

---
+++ simple-atm/atm.erl
@@ -0,0 +1,18 @@
+-module(atm).
+
+-export([]).
+
+%% atm:start() -> 
+%%     started
+
+%% atm:check_balance(AccountNumber) -> 
+%%     {balance, Amount} | no_such_account | atm_closed
+
+%% atm:withdraw(AccountNumber, Amount) -> 
+%%     {new_balance, Amount} | overdrawn | no_such_account | atm_closed
+
+%% atm:deposit(AccountNumber, Amount) -> 
+%%     {new_balance, Amount} | atm_closed
+
+%% atm:stop() -> 
+%%     stopped | atm_closed
